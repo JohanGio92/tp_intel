e2c3563102515a1b39639a4ed01bcf90cbbbe248
Juan Pablo Capurro <juanpcapurro@gmail.com>
2017-05-23 16:09:37 -0300

    hice un cat con el nombre de archivo hardcodeado

--- hello.asm
+++ hello.asm
@@ -3,20 +3,58 @@ segment pila stack
 
 segment datos data
 filename:   db  'history.txt',0
+errormsg:   db  'Error al abrir el archivo. Saliendo.$'
+fileHandle: resw 1
+fileBuffer: resb 1
 
 segment code
 ..start:
-    mov ax,datos
-    mov ds,ax
-    mov ax,pila
-    mov ss,ax
+    mov     ax,datos
+    mov     ds,ax
+    mov     ax,pila
+    mov     ss,ax
+    call    fOpen
+    call    loopOverFile
+    call    fClose
+    jmp     fin
 
-    mov dx,filename
-    mov al,0
-    mov ah,3dh
-    int 21h
+loopOverFile:
+    mov     bx,[fileHandle]
+    mov     cx,1
+    mov     dx,fileBuffer
+    mov     ah,3fh
+    int     21h
+    jc      fileError   ;no es error al abrir pero xd
+    cmp     ax,1
+    jne     onEOF
+    mov     dl,[fileBuffer]
+    mov     ah,2
+    int     21h
+    jmp     loopOverFile
+onEOF:
+    ret
 
+fileError:
+    mov     dx,errormsg
+    mov     ah,9
+    int     21h
+    jmp     fin
+
+fOpen:
+    mov     dx,filename
+    mov     al,0
+    mov     ah,3dh
+    int     21h
+    jc      fileError
+    mov     [fileHandle],ax
+    ret
+
+fClose:
+    mov     bx,[fileHandle]
+    mov     ah,3eh
+    int     21h
+    ret
 
 fin:
-    mov ax,4c00h
-    int 21h
+    mov     ax,4c00h
+    int     21h
